553a9140e96b5bfb6b020e0ed91795d006df5a36
LNCarrion <lestycarrion@gmail.com>
2023-10-16 13:26:34 -0400

    random numbers

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,14 +19,14 @@ int main(void)
 	/* your code goes there */
 	return (0);
 
-	if ( n > 0) {
+	if (n > 0)
 	printf("%d is positive/", n)
-	}
-	if ( n == 0) {
+
+	else
 	printf("%d is zero/", n)
-	}
-	if ( n < 0) {
+
+	else if (n < 0)
 	printf("%d is negative/", n)
-	}
+
 
 }
