1c5d8fddfada321aa51b3e758929bed5d543478c
LNCarrion <lestycarrion@gmail.com>
2023-10-15 22:37:00 -0400

    printingwith printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - print the string
+ *
+ * Description: with the main funcion the program will print
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
