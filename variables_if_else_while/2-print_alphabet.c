dc2509e02932f7ccb1f60dc81076b686545129af
LNCarrion <lestycarrion@gmail.com>
2023-10-17 11:22:35 -0400

    from a to z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print a random number
+*
+* Description:determine if the random number is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	return (0);
+}
