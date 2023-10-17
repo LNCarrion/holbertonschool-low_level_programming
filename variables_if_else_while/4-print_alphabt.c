ac421e1b3b27bdd69069073746645d7197602205
LNCarrion <lestycarrion@gmail.com>
2023-10-17 14:02:03 -0400

    the alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print the alphabet
+*
+* Description:listing the alphabet in lowercase and uppercase
+* Return: 0
+*/
+
+int main(void)
+{
+	char l;
+
+	for
+		(l = 'a'; l <= 'z'; l++);
+	if
+		(l != 'q' && l != 'e');
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
