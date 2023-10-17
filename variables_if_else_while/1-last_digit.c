80381f5c64cca0da2f5d217f9dc07d9d0a2135ba
LNCarrion <lestycarrion@gmail.com>
2023-10-17 10:44:13 -0400

    calculating a remainder of a integer

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print a random number
+*
+* Description:determine if the random number is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
+
+	printf("the last digit of %d is %d ", n, l);
+
+	if (l < 5)
+		printf("is greater that 5\n");
+	else if (l == 0)
+		printf("and is 0\n");
+	else if (l < 6)
+		printf("and is less than 6 but not 0\n");
+	return (0);
+}
+
