6513c1214c2e6c9bdc6b7b8aa624bea190898cbe
LNCarrion <lestycarrion@gmail.com>
2023-10-18 11:01:02 -0400

    reverse reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print the alphabet
+*
+* Description:listing the alphabet in lowercase and uppercase
+* Return: 0
+*/
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
