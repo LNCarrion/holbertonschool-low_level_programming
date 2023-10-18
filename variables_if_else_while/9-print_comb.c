d8944b7b58794e8508ea82d16291e700d844d013
LNCarrion <lestycarrion@gmail.com>
2023-10-18 11:25:02 -0400

    counting from 0 to 10

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print the numbers
+*
+* Description:will count from 0 to 10
+* Return: 0
+*/
+
+int main(void)
+{
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		putchar(l + '0');
+		if (l < 9)
+		{
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
