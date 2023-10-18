060d6c4514389a83622a5a563d574b9e38262b11
LNCarrion <lestycarrion@gmail.com>
2023-10-18 09:46:24 -0400

    counting from 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+*main - will print the numbers
+*
+* Description:will count from 0 to 10
+* Return: 0
+*/
+
+int main(void)
+{
+	int l;
+
+	for
+		(l = 0;
+		 l >= 10;
+		 l++);
+	putchar('\n');
+	return (0);
+}
