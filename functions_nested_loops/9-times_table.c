6907f313a1d0d1bc4f8d6b5f104066f383033804
LNCarrion <lestycarrion@gmail.com>
2023-10-22 13:56:11 -0400

    the time tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *times_table - will print time times table
+ *
+ * @i - will be a product
+ * @j - will be a product
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+	product = (i * j);
+	if (product < 10 && j != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(product + '0');
+	}
+	else if (j != 0)
+	{
+		_putchar(' ');
+		_putchar((product / 10) + '0');
+		_putchar((product % 10) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	if (j < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+	}
+	}
+}
+
