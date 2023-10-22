df6773717fe206954f845d60ebebbc0ee04a6e7c
LNCarrion <lestycarrion@gmail.com>
2023-10-22 18:03:53 -0400

    the time tables

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - will add 2 integers
+ *
+ * @a - is an integer in the code
+ * @b - is an integer in the code
+ * return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
