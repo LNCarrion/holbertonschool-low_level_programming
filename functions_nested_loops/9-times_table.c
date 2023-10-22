52037f3cd5dff96c09378667daf0fe8806eaafac
LNCarrion <lestycarrion@gmail.com>
2023-10-22 17:42:25 -0400

    the time tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,38 +11,31 @@
 void times_table(void)
 {
 	int i, j, product;
-
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-	product = (i * j);
-	if (product < 10)
+	product = i * j;
+	if (j == 0)
 	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(product "2d%" + '0');
-	}
-	else if (j != 0)
-	{
-		_putchar(' ');
-		_putchar((product / 10) + '0');
-		_putchar((product", 2d%" 10) + '0');
+		_putchar('0' + product);
 	}
 	else
 	{
-		_putchar('0');
-	}
-	if (j < 9)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	else
-	{
-	_putchar('\n');
-	}
-	}
+		_putchar(',');
+		_putchar(' ');
+		if (product < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + product);
+		}
+		else
+		{
+			_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
 	}
 }
-
+_putchar('\n');
+}
+}
