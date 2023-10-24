89633df8d7929bf7a822b7377ba5e33da686738a
LNCarrion <lestycarrion@gmail.com>
2023-10-24 10:28:42 -0400

    2, 4, 6, 8, ...

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - will put character of a string
+ *
+ *@str: will be the string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	i += 2;
+	}
+_putchar('\n');
+}
