60f21b3e33db025c942a40c133b84f0b6eceb79d
LNCarrion <lestycarrion@gmail.com>
2023-10-25 10:42:11 -0400

    printing an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - will print the elements of "n"
+ * @a: is a variable
+ * @n: is a variable
+ *
+ * Return: will not print if n is negative
+ *
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n <= 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
