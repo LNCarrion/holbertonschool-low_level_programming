d3e3d6cdb965fefc25cadf5eedf65fe8df088509
LNCarrion <lestycarrion@gmail.com>
2023-10-31 10:27:08 -0400

    factoring numbers

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - Return the factorial of a number
+ * @n: the number for factorial
+ *
+ * Return: the factorial number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
