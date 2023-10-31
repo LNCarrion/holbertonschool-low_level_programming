32f290babce17798ae4e57517f168df8be0f1401
LNCarrion <lestycarrion@gmail.com>
2023-10-31 13:27:12 -0400

    square root of

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,25 +9,25 @@
 
 int find_square_root(int root, int n)
 {
-    int perfect_square = root * root;
+	int perfect_square = root * root;
 
-    if (perfect_square < n)
-    {
-        return (find_square_root(root + 1, n));
-    }
-    else if (perfect_square > n)
-    {
-        return (-1);
-    }
-    return (root);
+	if (perfect_square < n)
+	{
+		return (find_square_root(root + 1, n));
+	}
+	else if (perfect_square > n)
+	{
+		return (-1);
+	}
+	return (root);
 }
 
 /**
  * _sqrt_recursion - return the natural square root of a number
  * @n: random number to find square root
  * Return: the square root
-*/
+ */
 int _sqrt_recursion(int n)
 {
-    return (find_square_root(0, n));
+	return (find_square_root(0, n));
 }
