91578cd2184bae11790af476554df7cbc122fe4b
LNCarrion <lestycarrion@gmail.com>
2023-11-15 14:08:17 -0400

    woof woof

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about a dog
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (float)
+ * @owner: The owner of the dog (string)
+ *
+ * Description: This structure defines the properties of a dog,
+ * including its name, age, and owner.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
