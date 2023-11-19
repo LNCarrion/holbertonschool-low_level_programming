6e30894d1307dd95e14fd29c988a18a89cf86b36
lestynomar <lestynomar@penguin>
2023-11-19 13:06:04 -0400

    pitagoras would be proud

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,35 +3,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: 0 on success, other values on failure
+ */
+
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*func)(int, int);
-    char buffer[12];
-    int len;
+	int num1, num2, result;
+	int (*func)(int, int);
+	char buffer[12];
+	int len;
 
-    if (argc != 4)
-    {
-        write(STDOUT_FILENO, "Error\n", 6);
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		write(STDOUT_FILENO, "Error\n", 6);
+		exit(98);
+	}
 
-    func = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-    if (func == NULL)
-    {
-        write(STDOUT_FILENO, "Error\n", 6);
-        exit(99);
-    }
+	if (func == NULL)
+	{
+		write(STDOUT_FILENO, "Error\n", 6);
+		exit(99);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    
-    result = func(num1, num2);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    len = sprintf(buffer, "%d\n", result);
-    write(STDOUT_FILENO, buffer, len);
+	result = func(num1, num2);
 
-    return (0);
+	len = sprintf(buffer, "%d\n", result);
+	write(STDOUT_FILENO, buffer, len);
+
+	return (0);
 }
 
