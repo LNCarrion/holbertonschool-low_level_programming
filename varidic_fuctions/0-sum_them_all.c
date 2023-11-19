6608963478c7c45fab382a73df6205a57ecce7ba
lestynomar <lestynomar@penguin>
2023-11-19 15:25:43 -0400

    adding parameters

---
+++ varidic_fuctions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of its parameters
+ * @n: Number of parameters
+ * @...: Variable number of parameters
+ *
+ * Return: Sum of parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	va_list args;
+	unsigned int i;
+	
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
+	return (sum);
+}
