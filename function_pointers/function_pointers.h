d328c132bfcf89253c7033da9569ba3a052c72ea
lestynomar <lestynomar@penguin>
2023-11-19 11:32:02 -0400

    the parameters

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 /**
  * print_name - prints a name
  * @name: name of the person
@@ -9,5 +11,15 @@
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to the function to be executed on each element
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
