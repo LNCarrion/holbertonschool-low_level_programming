19d8eda0a740a1ebdba495221c27eb40fd371b43
lestynomar <lestynomar@penguin>
2023-11-30 13:29:29 -0400

    this first

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * startup_message - Prints the startup message.
+ */
+void startup_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
