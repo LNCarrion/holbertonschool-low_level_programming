4f1b7828ab3aba7e07ef74b7f8b352f128a974c9
lestynomar <lestynomar@penguin>
2023-12-07 13:31:40 -0400

    reading a file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: The actual number of letters it could read and print.
+ *         If the file cannot be opened or read, return 0.
+ *         If filename is NULL, return 0.
+ *         If write fails or does not write the expected amount of bytes, return 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *buffer;
+	ssize_t bytesRead, bytesWritten;
+
+	if (filename == NULL)
+		return (0);
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	bytesRead = fread(buffer, 1, letters, file);
+	if (bytesRead < 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten < 0 || (size_t)bytesWritten != (size_t)bytesRead)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	fclose(file);
+	free(buffer);
+
+	return (bytesRead);
+}
+
