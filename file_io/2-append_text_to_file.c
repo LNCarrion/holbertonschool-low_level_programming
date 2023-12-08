1cc0a613615bf004d55a9c286193faac74819edd
lestynomar <lestynomar@penguin>
2023-12-08 09:25:31 -0400

    append (betty fixed)

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i=0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i] != '\0'; i++)
 			;
 
 		write_status = write(fd, text_content, i);
