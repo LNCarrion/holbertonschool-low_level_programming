1cfa6974074020b19a865ae1e41341c50ec7d0fd
lestynomar <lestynomar@penguin>
2023-12-10 12:56:05 -0400

    adding elements to the hash tables(betty fixed)

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,10 +3,47 @@
 #include <string.h>
 
 /**
+ * create_node - Creates a new hash node.
+ * @key: The key (cannot be an empty string).
+ * @value: The value associated with the key
+ * (duplicated, can be an empty string).
+ *
+ * Return: A pointer to the new hash node, or NULL on failure.
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table to update or add the key/value to.
  * @key: The key (cannot be an empty string).
- * @value: The value associated with the key (duplicated, can be an empty string).
+ * @value: The value associated with the key
+ * (duplicated, can be an empty string).
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
@@ -35,24 +72,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current_node = current_node->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
