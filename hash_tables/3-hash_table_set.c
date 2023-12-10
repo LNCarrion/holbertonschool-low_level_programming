69cf9b72c3809144c593d8329878a2ac008297d5
lestynomar <lestynomar@penguin>
2023-12-10 12:48:26 -0400

    adding elements to the hash tables

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,17 +10,17 @@
  *
  * Return: 1 if it succeeded, 0 otherwise.
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node, *current_node;
 
-	if(ht == NULL || key == NULL || *key == '\0')
-		return(0);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	current_node = ht->array[index];
+
 
 	while (current_node != NULL)
 	{
@@ -32,13 +32,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
-
 		current_node = current_node->next;
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
@@ -47,8 +53,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
 }
+
